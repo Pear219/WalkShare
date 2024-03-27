26d934f437bf05a3dd13c0ada932435f26f495ca
Kokona Kato <koko72219@yahoo.co.jp>
2024-03-27 23:38:41 +0900

    Initial Commit

---
+++ WalkShare/WalkShareApp.swift
@@ -0,0 +1,17 @@
+//
+//  WalkShareApp.swift
+//  WalkShare
+//
+//  Created by 加藤 on 2024/03/27.
+//
+
+import SwiftUI
+
+@main
+struct WalkShareApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
