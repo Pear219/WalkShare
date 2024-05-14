ed37ff6f3357aee66a6317337eb24627a0284b79
Kokona Kato <koko72219@yahoo.co.jp>
2024-05-15 00:02:38 +0900

    [add]TutorialView,PageTabViewの追加(チュートリアル自体は未実装)

---
+++ WalkShare/TutorialView.swift
@@ -0,0 +1,18 @@
+//
+//  TutorialView.swift
+//  WalkShare
+//
+//  Created by Kokona Kato on 2024/05/14.
+//
+
+import SwiftUI
+
+struct TutorialView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TutorialView()
+}
